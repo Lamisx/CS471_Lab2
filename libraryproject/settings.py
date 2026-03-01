d3eba4120e1a51369e1859988bb52ded0fc20856
Lamisx <lamprog01@gmail.com>
2026-03-01 20:29:30 +0300

    Add initial templates, styles, and images for book module
    - Created a base layout template with header and footer includes.
    - Added navigation links in the header with active link styling.
    - Implemented an About Us page with basic content.
    - Developed a List of Books page with links and images for each book.
    - Created a detailed view for a single book with description and image.
    - Added CSS styles for navigation and book list presentation.
    - Included a new icon image for the website.

--- libraryproject/settings.py
+++ libraryproject/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-       'apps.bookmodule',
-       'apps.usermodule',
+    'apps.bookmodule',
+    'apps.usermodule',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "apps/static")]
